cd7fc93ba8ebba0697e2cdbb9836592954014d4d
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-20 16:52:34 -0500

    Update Exercise_3.r

--- Chapter 1/1.2/Exercise_3.r
+++ Chapter 1/1.2/Exercise_3.r
@@ -1,1 +1,12 @@
-
+ggplot(
+    data = penguins,
+    mapping = aes(x = bill_length_mm, y = bill_depth_mm)
+) +
+    geom_point(mapping = aes(shape = species, color = species)) +
+    geom_smooth(method = "lm") +
+    labs(
+        title = "Bill depth and bill length",
+        subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo penguins",
+        color = "Species", shape = "Species"
+    ) +
+    scale_color_colorblind()
